be6e110ea0d3bcccc91c78fed5e316248e5968d7
Yoshinori Takeshima <yotakeshima@gmail.com>
2025-01-23 00:53:51 -0500

    added a product list to the homepage. Also limited the product viewing to only four

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
+
 const Homepage = () => {
-  return <>Poke store</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Recently Listed"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,5 +1,30 @@
-const ProductList = ({ data, title }: { data: any; title?: String }) => {
-  return <div></div>;
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className="my-10">
+      <h2 className="mb-4 h2-bold">{title}</h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product: any) => (
+            <div>{product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProductList;
