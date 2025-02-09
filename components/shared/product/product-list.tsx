d668b1d855c207269cfe4e6b22da189f6fb5ad0d
Yoshinori Takeshima <yotakeshima@gmail.com>
2025-02-09 08:25:37 -0500

    created a smaller container for larger screens

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -18,9 +18,9 @@ const ProductList = async ({
 
   return (
     <div className="my-10">
-      <h2 className="mb-4 h2-bold">{title}</h2>
+      <h2 className="mb-4 h2-bold ml-10">{title}</h2>
       {data.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 lg:max-w-[85vw] mx-auto items-center">
           {limitedData.map((product: Product) => (
             <ProductCard
               key={product.id}
