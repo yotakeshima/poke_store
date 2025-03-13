f444292fb0d2d860cc59b83e2a4dec1fb6076c10
Yoshinori Takeshima <yotakeshima@gmail.com>
2025-03-12 23:07:19 -0400

    added a checkout-steps component

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -5,6 +5,7 @@ import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 import { ShippingAddress } from '@/types';
 import ShippingAddressForm from './shipping-address-form';
+import CheckoutSteps from '@/components/shared/checkout-steps';
 
 export const metadata: Metadata = {
   title: 'Shipping Address',
@@ -24,6 +25,7 @@ const ShippingAddressPage = async () => {
   const user = await getUserById(userId);
   return (
     <>
+      <CheckoutSteps current={1} />
       <ShippingAddressForm address={user.address as ShippingAddress} />
     </>
   );
