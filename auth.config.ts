8d7954be54d0850d6217885b9d60b621c4e03bce
Yoshinori Takeshima <yotakeshima@gmail.com>
2025-03-13 00:34:30 -0400

    created a protected paths

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,23 @@ export const authConfig = {
   providers: [],
   callbacks: {
     authorized({ request, auth }: any) {
+      // Array of regex patterns of paths to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       // Check for session cart cookie
       if (!request.cookies.get('sessionCartId')) {
         // Generate new session cart id cookie
--- auth.ts
+++ auth.ts
@@ -76,6 +76,7 @@ export const config = {
     async jwt({ token, user, trigger, session }: any) {
       // Assign user fields to token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
 
         // If user has no name, then use the email
@@ -89,6 +90,31 @@ export const config = {
               name: token.name,
             },
           });
+        }
+
+        // Persists User Cart
+        if (trigger === 'signIn' || trigger === 'signout') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
         }
       }
       return token;
