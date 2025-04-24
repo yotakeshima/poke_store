ffc8426ddce07b258afa990892e073a6dd40d3f3
Yoshinori Takeshima <yotakeshima@gmail.com>
2025-04-23 21:47:57 -0400

    added a link to the user profile and orders in dropdown menu

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -49,6 +49,19 @@ const UserButton = async () => {
               </div>
             </div>
           </DropdownMenuLabel>
+
+          <DropdownMenuItem>
+            <Link href="/user/profile" className="w-full">
+              User Profile
+            </Link>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <Link href="/user/orders" className="w-full">
+              Order History
+            </Link>
+          </DropdownMenuItem>
+
           <DropdownMenuItem className="p-0 mb-1">
             <form action={signOutUser} className="w-full">
               <Button
